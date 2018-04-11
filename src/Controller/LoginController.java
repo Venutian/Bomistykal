8b1a7dd5bff0b3c5c2045356bd013d07b305d02f
andreas1995 <suidos999@gmail.com>
2018-04-11 20:40:54 +0200

    fxml

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -1,4 +1,28 @@
 package Controller;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class LoginController {
+	
+	
+	public void login(ActionEvent event) throws IOException {
+		System.out.println("dddd");
+		Parent root = FXMLLoader.load(getClass().getResource("/View/Menu.fxml"));
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("/View/application.css").toExternalForm());
+		Stage primaryStage = new Stage();
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	
+	
+	
+	
 }
--- src/Controller/MenuController.java
+++ src/Controller/MenuController.java
@@ -1,4 +1,53 @@
 package Controller;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class MenuController {
+	
+
+	public void CheckIn(ActionEvent event) throws IOException {
+		System.out.println("CheckIn");
+		Parent root = FXMLLoader.load(getClass().getResource("/View/CheckIn.fxml"));
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("/View/application.css").toExternalForm());
+		Stage primaryStage = new Stage();
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	public void CheckOut(ActionEvent event) throws IOException {
+		System.out.println("CheckOut");
+		Parent root = FXMLLoader.load(getClass().getResource("/View/CheckOut.fxml"));
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("/View/application.css").toExternalForm());
+		Stage primaryStage = new Stage();
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	public void Reserve(ActionEvent event) throws IOException {
+		System.out.println("Reserve");
+		Parent root = FXMLLoader.load(getClass().getResource("/View/Reserve.fxml"));
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("/View/application.css").toExternalForm());
+		Stage primaryStage = new Stage();
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	public void Cancel(ActionEvent event) throws IOException {
+		System.out.println("Cancel");
+		Parent root = FXMLLoader.load(getClass().getResource("/View/Cancel.fxml"));
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("/View/application.css").toExternalForm());
+		Stage primaryStage = new Stage();
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	
+	
 }
