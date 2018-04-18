0b49d6ce9ab58894a096aeae943639f37c4e95c2
WaeelAlhrazy <wael_abbas11@hotmail.com>
2018-04-18 19:56:38 +0200

    Fixing SearchRoom fxml

--- src/Controller/SearchRoomController.java
+++ src/Controller/SearchRoomController.java
@@ -6,100 +6,72 @@ import javafx.stage.*;
 import sun.applet.Main;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 
 
-public class SearchRoomController extends Application {
+public class SearchRoomController implements Initializable{
 	
-	@FXML
-    private BorderPane boarderPane;
-	@FXML
-    private AnchorPane top;
+	 @FXML
+	    private AnchorPane anchor;
 
-    @FXML
-    private Label labSet;
+	    @FXML
+	    private ChoiceBox<String> campusLoc;
 
-    @FXML
-    private ChoiceBox<String> campusLocation;
+	    @FXML
+	    private DatePicker checkIn;
 
-    @FXML
-    private DatePicker CheckInDate;
+	    @FXML
+	    private DatePicker checkOut;
 
-    @FXML
-    private DatePicker CheckOutDate;
+	    @FXML
+	    private ChoiceBox<Integer> roomsPicker;
 
-    @FXML
-    private ChoiceBox<Integer> roomsPicker;
+	    @FXML
+	    private ChoiceBox<Integer> guestPicker;
 
-    @FXML
-    private ChoiceBox<Integer> guestPicker;
+	    @FXML
+	    private CheckBox roomType1;
 
-    @FXML
-    private CheckBox roomType1; // variable for smoking room
+	    @FXML
+	    private CheckBox roomType2;
 
-    @FXML
-    private CheckBox roomType2; // variable for non-smoking room
+	    @FXML
+	    private CheckBox roomType3;
 
-    @FXML
-    private CheckBox roomType3; // variable for allowed pets room 
+	    @FXML
+	    private CheckBox roomType4;
 
-    @FXML
-    private CheckBox roomType4;// variable for adjoint  room
+	    @FXML
+	    private CheckBox bedType1;
 
-    @FXML
-    private CheckBox bedType1; // variable for double bed room
+	    @FXML
+	    private CheckBox bedType2;
 
-    @FXML
-    private CheckBox bedType2; // variable for twin bed room
+	    @FXML
+	    private TableView<?> tabView;
 
-    @FXML
-    private AnchorPane mid;
+	    @FXML
+	    private TableColumn<?, ?> tabCol_Des;
 
-    @FXML
-    private TableView<?> tabView;
+	    @FXML
+	    private TableColumn<?, ?> tabCol_Id;
 
-    @FXML
-    private TableColumn<?, ?> tabCol_Des;
+	    @FXML
+	    private TableColumn<?, ?> tabCol_Availble;
 
-    @FXML
-    private TableColumn<?, ?> tabCol_Id;
+	    @FXML
+	    private Label roomDetails;
 
-    @FXML
-    private TableColumn<?, ?> tabCol_Availble;
-
-    @FXML
-    private AnchorPane left;
-	
-    /*@Override
-	public void start(Stage primaryStage) throws IOException {
-    	 FXMLLoader loader = new FXMLLoader();
-    	loader.setLocation(Main.class.getResource("/View/SearchRoom.fxml"));
-        boarderPane = loader.load();
-        Scene sc = new Scene(boarderPane);
-	    
-    	// set the choice for campus location choice box button
-      this.campusLocation = new ChoiceBox<String>(FXCollections.observableArrayList("Växjö" , "Kalmar"));
-      
-        // set the number of rooms for combo box
-      this.roomsPicker = new ChoiceBox<Integer>(FXCollections.observableArrayList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20));
-      
-        // set the number of guests for combo box
-        this.guestPicker = new ChoiceBox<Integer>(FXCollections.observableArrayList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20));
-   
-        
-        primaryStage.setTitle("Book Search");
-        primaryStage.setScene(sc);
-        primaryStage.show();
-        
-	} */
-    
     
     // Handles all the choice boxes in search room 
     @FXML
@@ -137,13 +109,21 @@ public class SearchRoomController extends Application {
     	    		
     	    	}
     	 }
-	public static void main(String[] args) {
-		
-		launch(args);
-	}
+
+
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// set the choice for campus location choice box button
+	      this.campusLoc = new ChoiceBox<String>(FXCollections.observableArrayList("Växjö" , "Kalmar"));
+	      
+	        // set the number of rooms for combo box
+	      this.roomsPicker = new ChoiceBox<Integer>(FXCollections.observableArrayList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20));
+	      
+	        // set the number of guests for combo box
+	        this.guestPicker = new ChoiceBox<Integer>(FXCollections.observableArrayList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20));
+	   
 		
 	}
+
+	
 }
