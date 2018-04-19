f929685f2e20eb22b57c07e5b165a950370eb08c
andreas1995 <suidos999@gmail.com>
2018-04-19 18:32:21 +0200

    Client

--- src/Model/Client.java
+++ src/Model/Client.java
@@ -3,8 +3,59 @@ package Model;
 public class Client {
 	/*name,address, telephone number, credit card, passport number,*/
 	private String name;
-	private String ID;
-	private String creditCardNumber;
+	private int creditCardNumber;
+	private int phoneNumber;
+	private String IDNumber;
+	private String address;
+	
+	public Client (String name,String IDNumber, int creditCardNumber,int phoneNumber,String address) {
+		this.name = name;
+		this.IDNumber = IDNumber;
+		this.creditCardNumber = creditCardNumber;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+	}
 
+	
+	/*setters*/
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setIDNumber(String id) {
+		this.IDNumber = id;
+	}
+	public void setCreditCardNum(int crn) {
+		this.creditCardNumber = crn;
+	}
+	public void setPhoneNumber(int pnum) {
+		this.phoneNumber = pnum;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	/*getters*/
+	public String getName() {
+		return this.name;
+	}
+	public String getIDNumber() {
+		return this.IDNumber;
+	}
+	public int getCreditCardNum() {
+		return this.creditCardNumber;
+	}
+	public int getPhoneNumber() {
+		return this.phoneNumber;
+	}
+	public String getAddress() {
+		return this.address;
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 }
