338b667d10fdb3139050cf444d1bc9f16b7e1f72
WaeelAlhrazy <wael_abbas11@hotmail.com>
2018-04-19 18:49:46 +0200

    fixed room

--- src/Model/Room.java
+++ src/Model/Room.java
@@ -6,20 +6,39 @@ public class Room {
 	private int price;
 	private String roomNumber;
 	private int floor;
+	private int roomSize;
+	private int numOfBed;
 	
-	private boolean isAvailable;
+	private enum RoomType {
+		Smoking,
+		Non_Smoking,
+		View,
+		Adjoint,
+		Pets_Allowed;
+	}
+	private enum BedType{
+		Single,
+		Double,
+		Twin,
+	}
 	
-	public Room (int floor, String roomNumber, String desc, int price ) {
+	public Room (int floor, String roomNumber, String desc, int price, int roomSize, int numOfBed, BedType bd, RoomType rt ) {
 		this.desc = desc;
 		this.floor = floor;
 		this.price = price;
+		this.roomNumber = roomNumber;
+		this.roomSize = roomSize;
+		this.numOfBed = numOfBed;
+		
+		
+		
 		
 	} 
-	public int getId() {
-		return id;
+	public int getFloor() {
+		return floor;
 	} 
-	public void setId(int id) {
-		this.id = id;
+	public void setFloor(int floor) {
+		this.floor = floor;
 	} 
 	public String getDescription() {
 		return desc;
@@ -33,6 +52,26 @@ public class Room {
 	public void setPrice(int price) {
 		this.price = price;
 	}
+	public String getRoomNumber() {
+		return roomNumber;
+	}
+	public void setRoomNumber(String roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	public int getNumOfBed() {
+		return numOfBed;
+	}
+	public void setNumOfBed(int numOfBed) {
+		this.numOfBed = numOfBed;
+		
+	}public int getRoomSize() {
+		return roomSize;
+	
+	} public void setRoomSize(int roomSize) {
+		this.roomSize = roomSize;
+	}
+	
+		
 	
 	
 } 
