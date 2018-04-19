a54784a76a4e93fb33f1a4ba551245e24accbb2c
andreas1995 <suidos999@gmail.com>
2018-04-19 18:18:50 +0200

    reservation

--- src/Model/Client.java
+++ src/Model/Client.java
@@ -1,4 +1,10 @@
 package Model;
 
 public class Client {
+	/*name,address, telephone number, credit card, passport number,*/
+	private String name;
+	private String ID;
+	private String creditCardNumber;
+
+	
 }
--- src/Model/Reservation.java
+++ src/Model/Reservation.java
@@ -1,5 +1,69 @@
 package Model;
 
+import java.util.Date;
+
 public class Reservation {
 
+	Date checkIn;
+	Date checkOut;
+	Client client;
+	Room room;
+	Employee emp;
+	int totalPrice;
+	
+	public Reservation(Date checkIn,Date checkOut, Client client,Room room,Employee emp,int totalPrice) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+		this.client = client;
+		this.room = room;
+		this.emp = emp;
+		this.totalPrice = totalPrice;
+	}
+	
+	/*setters*/
+	public void setCheckInDate(Date date) {
+		this.checkIn = date;
+	}
+	
+   public void setCheckoutDate(Date date) {
+		this.checkOut = date;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+	public void setEmployee(Employee emp) {
+		this.emp = emp;
+	}
+	public void setTotalPrice(int price) {
+		this.totalPrice = price;
+	}
+	
+	
+	/*getters*/
+	public Date getCheckInDate() {
+		return this.checkIn;
+	}
+	
+	public Date getCheckOutDate() {
+		return this.checkOut;
+	}
+	public Client getClient() {
+		return this.client;
+	}
+	public Room getRoom() {
+		return this.room;
+	}
+	public Employee getEmployee() {
+		return this.emp;
+	}
+	public int getTotalPrice() {
+		return this.totalPrice;
+	}
+	
+	
+	
+	
 }
