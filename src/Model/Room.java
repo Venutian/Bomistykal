29faa4d587f59435b3768267a667b4209e811468
WaeelAlhrazy <wael_abbas11@hotmail.com>
2018-04-19 18:05:43 +0200

    add reservation class

--- src/Model/Room.java
+++ src/Model/Room.java
@@ -1,4 +1,38 @@
 package Model;
 
 public class Room {
-}
+	
+	private String desc;
+	private int price;
+	private String roomNumber;
+	private int floor;
+	
+	private boolean isAvailable;
+	
+	public Room (int floor, String roomNumber, String desc, int price ) {
+		this.desc = desc;
+		this.floor = floor;
+		this.price = price;
+		
+	} 
+	public int getId() {
+		return id;
+	} 
+	public void setId(int id) {
+		this.id = id;
+	} 
+	public String getDescription() {
+		return desc;
+	}
+	public void setDescription(String description) {
+		this.desc = description;
+	}
+	public int getPrice() {
+		return price;
+	} 
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+} 
