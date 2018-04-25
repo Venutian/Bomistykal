147186b49a8d06c54cebdb14a06c3dac1a46cb19
andreas1995 <suidos999@gmail.com>
2018-04-25 20:07:10 +0200

    dsa

--- src/Controller/ReserveController.java
+++ src/Controller/ReserveController.java
@@ -1,6 +1,35 @@
 package Controller;
 
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class ReserveController {
-	
-	
+
+    @FXML
+    private TextField IDnumber;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField adrress;
+
+    @FXML
+    private TextField telNumber;
+
+    @FXML
+    private TextField CreditCardNumber;
+
+    @FXML
+    private DatePicker CreditCardExpDate;
+
+    @FXML
+    void reserve(ActionEvent event) {
+
+    }
 }
+
