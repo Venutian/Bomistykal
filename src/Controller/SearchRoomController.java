0befa77b3c2215ad2515d053587bf6ad803a4e77
WaeelAlhrazy <wael_abbas11@hotmail.com>
2018-04-26 20:27:10 +0200

    reserve fxml

--- src/Controller/SearchRoomController.java
+++ src/Controller/SearchRoomController.java
@@ -7,7 +7,11 @@ import java.io.IOException;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
@@ -15,6 +19,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 
 
@@ -135,7 +140,19 @@ public class SearchRoomController {
 	   
 	   
 	        
-	} public void initMain(Main mainWindow) {
+	}  @FXML
+    void reservebtn(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/View/Reserve.fxml"));
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("/View/application.css").toExternalForm());
+		Stage primaryStage = new Stage();
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	
+	public void initMain(Main mainWindow) {
+	
 		this.mainWindow = mainWindow;
 		
 	}
