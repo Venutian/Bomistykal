a113d593d1ae112751353c4e913b2d2e29011fa4
WaeelAlhrazy <wael_abbas11@hotmail.com>
2018-04-28 16:38:05 +0200

    fix edit window

--- src/Controller/ReserveController.java
+++ src/Controller/ReserveController.java
@@ -2,8 +2,14 @@ package Controller;
 
 
 
+import java.io.IOException;
+import java.util.Observable;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
@@ -26,12 +32,25 @@ public class ReserveController {
 
     @FXML
     private DatePicker CreditCardExpDate;
+    
+    @FXML
+    private ChoiceBox<String> guestNumBox;
+    
+    ObservableList<String> guestNumber = FXCollections.observableArrayList("1","2","3","4","5","6","7","8","9","10");
 
     @FXML
     void reserve(ActionEvent event) {
      
     	
     }
+    
+    /*public void initialize() throws IOException{
+    
+    
+    this.guestNumBox.setItems(guestNumber);
+    guestNumBox.setValue("1");
+    	
+    }*/
     /*filter methods are checking the inputs if they are correct.
      * if they are not an alert window will pop-up saying what is wrong and why.*/
     private void filterTelNumber() {
