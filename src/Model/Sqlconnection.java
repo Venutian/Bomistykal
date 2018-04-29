3f43770ae45784f97d327793959b7821707fbf4d
andreas1995 <suidos999@gmail.com>
2018-04-29 22:22:39 +0200

    data

--- src/Model/Sqlconnection.java
+++ src/Model/Sqlconnection.java
@@ -10,14 +10,12 @@ import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 public class Sqlconnection{
 	public static void main(String[] args) throws Exception {
 		//int floor, String roomNumber, int price, int roomSize,String Location
-		
-	
-		
-		;
-		
 		 DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 		
 		
@@ -35,12 +33,16 @@ public class Sqlconnection{
 		
 	 Reservation re = new Reservation(ff,ff,"D","d","fff");
 	 Reservation re2 = new Reservation(ff2,ff2,"D","d","fff");
-	 getComingReservations() ;
+	
+	 addReservation(re2);
+	// addReservation(re);
+	// getComingReservations() ;
+	 deleteReservation(re2);
 	// addReservation(re);
 	 //addReservation(re2);
 	}
 
-
+	
 
 /*booom*/
 public static void addRoom(Room room) throws Exception {
@@ -49,6 +51,8 @@ public static void addRoom(Room room) throws Exception {
     PreparedStatement pre = con.prepareStatement("INSERT INTO Room (Floor,RoomID,Price,Location,Description) "
     		+ " VALUES ('"+room.getFloor()+"','"+room.getRoomNumber()+"','"+room.getPrice()+"','"+room.getLocation()+"','"+room.getDescription()+"');");
 	pre.executeUpdate();
+	pre.close();
+	con.close();
 }
 
 public void addEmployee(Employee eployee) throws Exception {
@@ -58,6 +62,8 @@ public void addEmployee(Employee eployee) throws Exception {
     PreparedStatement pre = con.prepareStatement("INSERT INTO Employee (Name,IDNumber,UserName,Password,Adrress,PhoneNumber,Manager) "
     		+ " VALUES ('"+eployee.getName()+"','"+eployee.getIDNumber()+"','"+eployee.getUserName()+"','"+eployee.getPassword()+"','"+eployee.getAddress()+"','"+eployee.getPhoneNumber()+"','"+eployee.isManager()+"');");
 	pre.executeUpdate();
+	pre.close();
+	con.close();
 }
 
 public static void addReservation(Reservation reservation) throws Exception {
@@ -71,24 +77,56 @@ public static void addReservation(Reservation reservation) throws Exception {
     PreparedStatement pre = con.prepareStatement("INSERT INTO Reservation (CheckIn, CheckOut, ClientID, RoomID, EmployeeUN, ReservationID)"
     		+ "  VALUES ('"+a+"','"+a+"','"+reservation.getClient()+"','"+reservation.getRoom()+"','"+reservation.getEmployee()+"','"+reservation.getReservationID()+"');");
 	pre.executeUpdate();
+	pre.close();
+	con.close();
 }
 
 
 /*get*/
 /*booom*/
-//SELECT * FROM Persons WHERE P_Id = 3
-public static void getComingReservations() throws Exception {
-	
+public static ObservableList<Reservation> getComingReservations() throws Exception {
+	ObservableList<Reservation> data =  FXCollections.observableArrayList();
 	Connection con = getConnection();
     PreparedStatement pre = con.prepareStatement("SELECT * FROM Reservation WHERE CheckIn >= CURDATE()");
     ResultSet rs = pre.executeQuery();
 	while(rs.next()) {
 		System.out.println(rs.getString("ReservationID"));
+		data.add((Reservation) rs);
 	}
-    
-    
-    
+	pre.close();
+	con.close();
+	return data;   
+	
 }
+
+public static ObservableList<Reservation> getTodayCheckIn() throws Exception {
+	ObservableList<Reservation> data =  FXCollections.observableArrayList();
+	Connection con = getConnection();
+    PreparedStatement pre = con.prepareStatement("SELECT * FROM Reservation WHERE CheckIn = CURDATE()");
+    ResultSet rs = pre.executeQuery();
+	while(rs.next()) {
+		System.out.println(rs.getString("ReservationID"));
+		data.add((Reservation) rs);
+	}
+	return data;   
+}
+
+public static ObservableList<Reservation> getTodayCheckOut() throws Exception {
+	ObservableList<Reservation> data =  FXCollections.observableArrayList();
+	Connection con = getConnection();
+    PreparedStatement pre = con.prepareStatement("SELECT * FROM Reservation WHERE CheckOut = CURDATE()");
+    ResultSet rs = pre.executeQuery();
+	while(rs.next()) {
+		System.out.println(rs.getString("ReservationID"));
+		data.add((Reservation) rs);
+	}
+	return data;   
+}
+
+
+
+
+
 
 
 
@@ -96,20 +134,20 @@ public static void getComingReservations() throws Exception {
 /*fixing*/
 public void deleteRoom(Room room) throws Exception{
 	Connection con = getConnection();
-    PreparedStatement pre = con.prepareStatement("INSERT INTO room (roomRow,RoomNumber,floor,price,roomsize,location,numofbeds,bedtype) "
-    		+ " VALUES ('"+1+"','"+room.getRoomNumber()+"','"+room.getFloor()+"','"+room.getPrice()+"','"+room.getRoomSize()+"','"+room.getLocation()+"',"
-    				+ "'"+room.getNumOfBed()+"','gfd');");
+    PreparedStatement pre = con.prepareStatement("DELETE FROM Room WHERE RoomID = '"+room.getRoomNumber()+"'");
 	pre.executeUpdate();
 }
 public void deleteEmployee(Employee employee) throws Exception{
 	Connection con = getConnection();
-    PreparedStatement pre = con.prepareStatement("INSERT INTO room (roomRow,IDRoom)  VALUES ('2','APOEL');");
+	PreparedStatement pre = con.prepareStatement("DELETE FROM Employee WHERE UserName = '"+employee.getUserName()+"';");
 	pre.executeUpdate();
 }
-public void deleteReservation(Room reservation) throws Exception{
+public static void deleteReservation(Reservation reservation) throws Exception{
 	Connection con = getConnection();
-    PreparedStatement pre = con.prepareStatement("INSERT INTO room (roomRow,IDRoom)  VALUES ('2','APOEL');");
+	System.out.println(reservation.getReservationID());
+	PreparedStatement pre = con.prepareStatement("DELETE FROM Reservation WHERE ReservationID = '"+reservation.getReservationID()+"';");
 	pre.executeUpdate();
+	con.close();
 }
 
 
@@ -123,12 +161,12 @@ public void deleteReservation(Room reservation) throws Exception{
 	  //sql7.freesqldatabase.com
 	  //sql7235306
 	  //ed5j4AGc2a
-	   Connection conn = DriverManager.getConnection("jdbc:mysql://sql7.freesqldatabase.com/sql7235306","sql7235306","ed5j4AGc2a");
+	  Connection  con = DriverManager.getConnection("jdbc:mysql://sql7.freesqldatabase.com/sql7235306","sql7235306","ed5j4AGc2a");
 	  
 	   
 	  
    System.out.println("Connected");
-   return conn;
+   return con;
   } catch(Exception e){System.out.println(e);}
   
   
